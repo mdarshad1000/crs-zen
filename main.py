802b041d87c1847874c1ed758a698230e5036554
Mohammad Arshad <mdarshad1000@gmail.com>
2024-09-08 22:46:32 +0530

    Add all

--- main.py
+++ main.py
@@ -1,53 +1,48 @@
 from fastapi import FastAPI
-import undetected_chromedriver as uc
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import json
-from selenium_stealth import stealth
-import uvicorn
-import os
+from curl_cffi import requests
+
 
 app = FastAPI()
 
+@app.get("/fetch-data")
+async def fetch_data():
+    cookies = {
+        'AMCVS_0D15148954E6C5100A4C98BC%40AdobeOrg': '1',
+        's_ecid': 'MCMID%7C81710461424735814132251758168733180272',
+        's_cc': 'true',
+        'ASP.NET_SessionId': 'ciimk4xwu5rhyhbebipfcmi2',
+        '__cfruid': 'a9722f8935700119f2bde35a08cef557a10e0fe0-1725492540',
+        's_sq': '%5B%5BB%5D%5D',
+        '__cf_bm': 'xhFhG5ywJ_78TDjmyt.XfyJS.5qwt7qip6zjgzWRMg8-1725814193-1.0.1.1-us5XUMq8kR.q40rkijeK5aCXzY.T2d.zp_vSL9xPYQzSFe1sQnQvzrHtTMGLgu1lyHi5.i5fGisd6lSOBFX4NA',
+        'AMCV_0D15148954E6C5100A4C98BC%40AdobeOrg': '179643557%7CMCIDTS%7C19974%7CMCMID%7C81710461424735814132251758168733180272%7CMCAAMLH-1726418996%7C12%7CMCAAMB-1726418996%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1725821396s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C5.5.0',
+    }
 
-def run_playwright():
-    options = Options()
-    options.headless = True
-    options.add_argument("--window-size=1920,1080")
-    # Specify the path to the Chrome binary
-    # chrome_binary_path = os.path.join(os.getcwd(), 'chrome', 'chrome')
-    # options.binary_location = chrome_binary_path
-    # chromedriver_path = os.path.join(os.getcwd(), 'chromedriver')
+    headers = {
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,ht;q=0.7',
+        # 'cookie': 'AMCVS_0D15148954E6C5100A4C98BC%40AdobeOrg=1; s_ecid=MCMID%7C81710461424735814132251758168733180272; s_cc=true; ASP.NET_SessionId=ciimk4xwu5rhyhbebipfcmi2; __cfruid=a9722f8935700119f2bde35a08cef557a10e0fe0-1725492540; s_sq=%5B%5BB%5D%5D; __cf_bm=xhFhG5ywJ_78TDjmyt.XfyJS.5qwt7qip6zjgzWRMg8-1725814193-1.0.1.1-us5XUMq8kR.q40rkijeK5aCXzY.T2d.zp_vSL9xPYQzSFe1sQnQvzrHtTMGLgu1lyHi5.i5fGisd6lSOBFX4NA; AMCV_0D15148954E6C5100A4C98BC%40AdobeOrg=179643557%7CMCIDTS%7C19974%7CMCMID%7C81710461424735814132251758168733180272%7CMCAAMLH-1726418996%7C12%7CMCAAMB-1726418996%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1725821396s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C5.5.0',
+        'priority': 'u=1, i',
+        'referer': 'https://crsreports.congress.gov/search/',
+        'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+    }
+
+    response = requests.get(
+        'https://crsreports.congress.gov/search/results?term=&r=59414168&orderBy=Date&navids=4294966212&pageNumber=3&',
+        cookies=cookies,
+        headers=headers,
+        impersonate='chrome120'
+    )
+
+    print(response.status_code)
+    return response.text
+
+if __name__ == '__main__':
+    import uvicorn 
     
-    driver = uc.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-
-    stealth(driver,
-            languages=["en-US", "en"],
-            vendor="Google Inc.",
-            platform="Win32",
-            webgl_vendor="Intel Inc.",
-            renderer="Intel Iris OpenGL Engine",
-            fix_hairline=True,
-            )
-    
-    driver.get("https://crsreports.congress.gov/search/results?term=&r=59285836&orderBy=Date&navids=4294966212&pageNumber=5")
-    pre_content = driver.page_source
-    # Use BeautifulSoup to parse the HTML content
-    soup = BeautifulSoup(pre_content, 'lxml')
-    pre_tag_content = soup.find('pre').text if soup.find('pre') else 'No <pre> tag found'
-    content = json.loads(pre_tag_content)
-
-    driver.quit()
-    return content
-
-
-@app.get("/scrape")
-def scrape():
-    content = run_playwright()
-    return {"content": content}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=1234)
